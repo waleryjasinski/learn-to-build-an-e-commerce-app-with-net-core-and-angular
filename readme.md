b0410ee012646391ca903b25953b6da01b44f560
szerem <szerem@go2.pl>
2020-07-29 21:24:09 +0200

    controler

--- readme.md
+++ readme.md
@@ -35,3 +35,6 @@ root
 dotnet ef database drop -p Infrastructure -s API
 dotnet ef migrations remove -p .\Infrastructure\ -s .\API\
 dotnet ef migrations add InitialCreate -p .\Infrastructure\ -s .\API\ -o Data/Migrations
+
+
+
--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
-    {      
+    {
       var products = await _repo.GetProductsAsync();
 
       return Ok(products);
@@ -28,11 +28,21 @@ namespace API.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      var product = await _repo.GetProductByIdAsync(id); 
+      var product = await _repo.GetProductByIdAsync(id);
 
       return Ok(product);
     }
 
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+    {
+      return Ok(await _repo.GetProductBrandsAsync());
+    }
 
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+      return Ok(await _repo.GetProductTypesAsync());
+    }
   }
 }
